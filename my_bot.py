98d326f35fb32ead7dded9f881f473ad9fc0b313
nakeshpiransu-prog <nakeshpiransu@gmail.com>
2026-07-25 14:45:07 +0530

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -1,3 +1,29 @@
+
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return 'Bot is alive!'
+
+
+def run():
+  port = int(os.environ.get('PORT', 8080))
+  app.run(host='0.0.0.0', port=port)
+
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
+
+
+keep_alive()
+
+
 import telebot
 import sqlite3
 import re
